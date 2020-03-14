4512316a58a491eabf96bc79616f6214e667fb87
Jeff-Gang <61744633+Jeff-Gang@users.noreply.github.com>
2020-03-14 12:10:26 -0500

    MacBook@Home

---
+++ Javascript/122/122-Loops.js
@@ -0,0 +1,10 @@
+var todos = [
+	"clean room",
+	"brush teeth",
+	"exercise",
+	"study javascript",
+	"eat healthy",
+];
+todos.forEach(function(todo){
+	console.log(todo);
+})
---
+++ Javascript/Function-MethodReview-cheat sheet.js
@@ -0,0 +1,35 @@
+// function declaration
+function newFunction () {
+
+}
+
+// Function expression
+var newFunction = function () {
+
+};
+
+// expression
+1+3;
+var a = 2;
+return true;
+
+// calling or invoking a function
+alert()
+newFunction (param1, param2);
+
+//assign av ariable
+var a = true;
+
+// function vs. method
+function thisIsAFunction () {
+
+}
+
+var obj = {
+	thisIsAMethod: function() {
+
+	}
+}
+
+thIsAFunction ()
+obj.thisIsAMethod ()
