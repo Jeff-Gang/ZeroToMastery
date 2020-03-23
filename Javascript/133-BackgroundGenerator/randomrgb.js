da816f189ffbf40d6563cdaf92a52952b487e91f
Jeff-Gang <61744633+Jeff-Gang@users.noreply.github.com>
2020-03-23 14:16:04 -0500

    March 23 exercises

---
+++ Javascript/133-BackgroundGenerator/randomrgb.js
@@ -0,0 +1,12 @@
+function getRandomRgb() {
+    var num = Math.round(0xffffff * Math.random());
+    var r = num >> 16;
+    var g = num >> 8 & 255;
+    var b = num & 255;
+    return 'rgb(' + r + ', ' + g + ', ' + b + ')';
+  }
+  
+  for (var i = 0; i < 10; i++) {
+    console.log(getRandomRgb());
+  }
+  
