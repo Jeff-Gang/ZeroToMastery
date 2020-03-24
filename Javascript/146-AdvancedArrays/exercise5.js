8a08e3ec3557a9670793c65df5da9570def9a05d
Jeff-Gang <61744633+Jeff-Gang@users.noreply.github.com>
2020-03-24 14:44:55 -0500

    Update exercise5.js

--- Javascript/146-AdvancedArrays/exercise5.js
+++ Javascript/146-AdvancedArrays/exercise5.js
@@ -29,14 +29,57 @@ const array = [
 
 //Create an array using forEach that has all the usernames with a "!" to each of the usernames
 
+// My ATTEMPT::
+let newArrayUserNames = []
+let newArray = array.forEach(user => {
+  newArray.push(username + "!");
+})
+console.log(newArrayUserNames);
+// // WRONG!
+
+// //Andrei's answer:
+// let newArray = []
+// array.forEach(user => {
+// 	let { username } = user;
+// 	username = username + "!";
+// 	newArray.push(username);
+// })
+// console.log(newArray);
+
+// // this solution also errored in my Chrome browser: 
+// // VM235:1 Uncaught SyntaxError: Identifier 'array' has already been declared
+// //     at <anonymous>:1:1
+// ???? // Don't know the real solution here....
+
 
 //Create an array using map that has all the usernames with a "? to each of the usernames
+// MY ATTEMPT
+const mapArray = array.map((username) => {
+  return username + "?";
+})
+console.log(mapArray);
 
+// Andrei's answer:
+// let newArray = []
+// array.forEach(user => {
+// 	let { username } = user;
+// 	username = username + "!";
+// 	newArray.push(username);
+// })
+// console.log(newArray);
 
 //Filter the array to only include users who are on team: red
-
+//My REWORKED solution
+const filterArray = array.filter(user => {
+  return.user.team === "red";
+})
+console.log(filterArray)
 
 //Find out the total score of all users using reduce
+const reduceArray = array.reduce((accumulator, user) => {
+  return accumulator + user.score
+}, 0);
+console.log(reduceArray);
 
 // (1), what is the value of i?
 // (2), Make this map function pure:
